6a9168039fc478674227e113f7fc7abad6cb2838
alexnguyen104 <vstar.alex104@gmail.com>
2026-02-17 21:28:25 +1100

    update task

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = []
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -42,27 +42,181 @@ def parse():
 
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
-def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+def parse_handwriting(recipeName: str) -> Union[str , None]:
+	# Replacing hyphens and underscores
+	recipeName = recipeName.replace('-', ' ')
+	recipeName = recipeName.replace('_', ' ')
 
+	# Check for other characters
+	for letter in recipeName:
+		if not letter.isalpha() and letter != ' ':
+			recipeName = recipeName.replace(letter, '')
+	
+	# Capitalising letters
+	recipeName = recipeName.title()
+
+	# One whitespace
+	recipeName = recipeName.strip()
+	recipeName = " ".join(recipeName.split())
+
+	# Check length
+	if len(recipeName) == 0:
+		return None
+	
+	return recipeName
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	data = request.get_json()
 
+	global cookbook
+	if not is_unique(data, cookbook):
+		return '', 400
+
+	if data["type"] == "recipe":
+		if not is_recipe_valid(data):
+			return '', 400
+
+	elif data["type"] == "ingredient":
+		if not is_ingredient_valid(data):
+			return '', 400
+	else:
+		return '', 400
+	
+	# Update to cookbook
+	cookbook.append(data)
+	
+	return '', 200
+
+def is_unique(data, cookbook):
+	for entry in cookbook:
+		if entry["name"] == data["name"]:
+			return False
+	return True
+
+def is_recipe_valid(data):
+	if "cookTime" in data:
+		return False
+
+	# Check if ingredient is duplicated
+	for i in range(len(data["requiredItems"]) - 1):
+		item = data["requiredItems"]
+		if item[i]["name"] == item[i + 1]["name"]:
+			return False
+		
+	return True
+
+def is_ingredient_valid(data):
+	if "requiredItems" in data:
+		return False
+	
+	if data["cookTime"] < 0:
+		return False
+	
+	return True
+
+@app.route('/gcookbook', methods=["GET"])
+def gcookbook():
+	global cookbook
+	return jsonify(cookbook)
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	global cookbook
+	name = request.args.get("name")
 
+	# Check if the recipe's name exists and the recipe's items exist in the cookbook
+	if not recipe_name_exists(name, cookbook) or not items_exist(name,cookbook):
+		return '', 400
+	
+	base_ingredient, cook_time = get_base_ingredient(name, cookbook)
 
+	recipe_summary = {
+		"name": name,
+		"cookTime": cook_time,
+		"ingredients": base_ingredient
+	}
+
+	return jsonify(recipe_summary), 200
+
+def recipe_name_exists(name, cookbook):
+	for entry in cookbook:
+		if entry["name"] == name and entry["type"] == "recipe":
+			return True
+		
+	return False
+
+def get_cookbook_index_by_name(name, cookbook):
+	index = None
+	for i, entry in enumerate(cookbook):
+		if entry["name"] == name:
+			index = i
+	
+	return index
+
+def items_exist(name, cookbook):
+	index = get_cookbook_index_by_name(name, cookbook)
+
+	for item in cookbook[index]["requiredItems"]:
+		for entry in cookbook:
+			if item["name"] == entry["name"]:
+				return True
+			
+	return False
+
+def get_base_ingredient_raw(name, cookbook):
+	ingredient_list_raw = []
+	index = get_cookbook_index_by_name(name, cookbook)
+
+	for item in cookbook[index]["requiredItems"]:
+		item_index = get_cookbook_index_by_name(item["name"], cookbook)
+
+		if cookbook[item_index]["type"] == "ingredient":
+			ingredient = {
+				"name": item["name"],
+				"quantity": item["quantity"],
+				"cookTime": cookbook[item_index]["cookTime"]
+			}
+			ingredient_list_raw.append(ingredient)
+		else:
+			ingredient_list_raw.extend(get_base_ingredient_raw(item["name"], cookbook))
+
+	return ingredient_list_raw
+			
+def get_base_ingredient(name, cookbook):
+	raw_list = get_base_ingredient_raw(name, cookbook)
+	edited_list = []
+	cook_time = get_cook_time(raw_list)
+
+	for i in range(len(raw_list)):
+		quantity = raw_list[i]["quantity"]
+
+		# Update the quantity for the duplicated ingredients and remove them from the raw list
+		for j in range(len(raw_list)):
+			if raw_list[i]["name"] == raw_list[j]["name"] and i != j:
+				quantity += raw_list[j]["quantity"]
+				raw_list.pop(j)
+
+		ingredient = {
+			"name": raw_list[i]["name"],
+			"quantity": quantity
+		}
+
+		edited_list.append(ingredient)
+
+	return edited_list, cook_time
+
+def get_cook_time(raw_list):
+	cook_time = 0
+	for i in range(len(raw_list)):
+		cook_time += raw_list[i]["cookTime"]
+
+	return cook_time
+	
 # =============================================================================
 # ==== DO NOT TOUCH ===========================================================
 # =============================================================================
